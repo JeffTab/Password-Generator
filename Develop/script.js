863401040edcda56da567bcf52265c2d7a6e6102
Jeff Tab <j.tab.jr@gmail.com>
2019-12-08 23:00:57 -0500

    finished almost everything, working on css

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,65 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+alert("Hello! This is a password generator.  I just need you to answer a few questions about the type of password.");
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+while (length < 8 || length > 128 || isNaN(length)) {
+  var length = prompt("The length can be between 8-128 characters.  How long would you like it?");
+  if (length < 8 || length > 128) {
+    alert("Keep it between 8 and 128.");
+  };
+  if (isNaN(length)) {
+    alert("Please chose a number.")
+  };
+};
+while (special != true && numeric != true && lower != true && upper != true) {
+  var special = confirm("Would you like special characters? i.e. ! # $");
+  var numeric = confirm("Would you like numeric characters? i.e. 1 2 3");
+  var lower = confirm("Would you like to use lower case characters?");
+  var upper = confirm("Would you like to use upper case characters?");
 
-  passwordText.value = password;
+  var passwordOptions = [];
 
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
+  if (special != true && numeric != true && lower != true && upper != true) {
+    alert("Please choose at least one option.")
+  }
+
+  if (special) {
+    passwordOptions = passwordOptions.concat("!", "(", ")", "-", ".", "?", "[", "]", "_", "'", "~", ";", ":", "@", "#", "$", "%", "^", "&", "*", "+", "+");
+  };
+
+  if (numeric) {
+    passwordOptions = passwordOptions.concat("1", "2", "3", "4", "5", "6", "7", "8", "9", "0");
+  };
+
+  if (lower) {
+    passwordOptions = passwordOptions.concat("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+  }
+
+  if (upper) {
+    passwordOptions = passwordOptions.concat("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+  }
+
+}
+var buildPassword = [];
+
+for (var i = 0; i < length; i++) {
+
+  var currentOption = passwordOptions[Math.floor(Math.random() * passwordOptions.length)];
+  buildPassword = buildPassword.concat(currentOption);
+
 }
 
-function copyToClipboard() {
-  // BONUS 
+var password = buildPassword.join("");
+
+document.getElementById("generate").addEventListener("click", function () {
+  document.getElementById("password").innerHTML = password;
+});
+
+function copyFunction() {
+  var copyText = document.getElementById("password");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  alert("Copied the text: " + copyText.value);
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// BONUS EVENT LISTENER
